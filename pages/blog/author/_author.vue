cb287aba422d446a2bd117926d74b7b745fe1404
Fajar Apriansyah <apriansyah.fajar94@gmail.com>
2022-05-25 19:32:13 +0700

    Update _author.vue

--- pages/blog/author/_author.vue
+++ pages/blog/author/_author.vue
@@ -4,7 +4,7 @@
       Author: {{ articles[0].author.name }}
     </h1>
     <p>Bio: {{ articles[0].author.bio }}</p>
-    <h3>Here are a list of articles by {{ articles[0].author.name }}:</h3>
+    <h3>Here are a list of articles by {{ getAuthorName }}:</h3>
     <ul>
       <li v-for="article in articles" :key="article.slug">
         <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
@@ -41,6 +41,11 @@ export default {
     formatDate (date) {
       const options = { year: 'numeric', month: 'long', day: 'numeric' }
       return new Date(date).toLocaleDateString('en', options)
+    },
+    getAuthorName () {
+      if (!this.articles[0]) return
+      if (!this.articles[0].author) return
+      return this.articles[0].author.name
     }
   }
 }
